48780d59f106c607f4b1d03722ec3089ae4dcb04
Gastón Berruet <gastonberruet@gmail.com>
2024-10-07 23:09:15 -0300

    Hola Mundo!

---
+++ chango-market/src/main/java/com/chango/market/ChangoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.chango.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangoMarketApplication.class, args);
+	}
+
+}
---
+++ chango-market/src/test/java/com/chango/market/ChangoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chango.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
