c9aac913749d345dea7ae9f1720a963e2f9fa036
Gastón Berruet <gastonberruet@gmail.com>
2024-10-12 11:38:51 -0300

    Implementar la anotación @Repository

--- chango-market/src/main/java/com/chango/market/persistence/ProductoRepository.java
+++ chango-market/src/main/java/com/chango/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.chango.market.persistence;
 
 import com.chango.market.persistence.crud.ProductoCrudRepository;
 import com.chango.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad, boolean estado) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
