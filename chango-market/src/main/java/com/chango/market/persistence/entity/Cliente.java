9cc017ea8c8563e5a97965a9e9241b46d880529c
Gastón Berruet <gastonberruet@gmail.com>
2024-10-11 23:52:30 -0300

    Mapear las tablas como clases

---
+++ chango-market/src/main/java/com/chango/market/persistence/entity/Cliente.java
@@ -0,0 +1,71 @@
+package com.chango.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Double celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Double getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Double celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
