d12f475b2c192c181362e2e75876ed5d8ad4a9d2
Gastón Berruet <gastonberruet@gmail.com>
2024-10-17 10:20:32 -0300

    Crear el repositorio de compras

---
+++ chango-market/src/main/java/com/chango/market/persistence/CompraRepository.java
@@ -0,0 +1,40 @@
+package com.chango.market.persistence;
+
+import com.chango.market.domain.Purchase;
+import com.chango.market.domain.repository.PurchaseRepository;
+import com.chango.market.persistence.crud.CompraCrudRepository;
+import com.chango.market.persistence.entity.Compra;
+import com.chango.market.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+
+    @Autowired
+    private PurchaseMapper purchaseMapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return purchaseMapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> purchaseMapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = purchaseMapper.toCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+
+        return purchaseMapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
--- chango-market/src/main/java/com/chango/market/persistence/entity/Compra.java
+++ chango-market/src/main/java/com/chango/market/persistence/entity/Compra.java
@@ -26,7 +26,7 @@ public class Compra {
     private String comentario;
     private Boolean estado;
 
-    @OneToMany(mappedBy = "compra")
+    @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
     private List<ComprasProducto> productos;
 
     @ManyToOne
@@ -80,4 +80,21 @@ public class Compra {
     public void setEstado(Boolean estado) {
         this.estado = estado;
     }
+
+    public List<ComprasProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ComprasProducto> productos) {
+        this.productos = productos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
 }
